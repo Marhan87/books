7e916d370affc4e837cae1dd8d00ad3a6d56780a
Marhan87 <112327671+Marhan87@users.noreply.github.com>
2024-04-21 15:27:09 +0200

    Hid api-key from github
    Hid api-key from github

--- src/services/useGetBooks.tsx
+++ src/services/useGetBooks.tsx
@@ -1,9 +1,10 @@
 import { useCallback } from "react";
+import { API_KEY } from "./API-key";
 
 export const useGetBooks = () => {
     const getBooks = useCallback(async (searchTerm: string) => {
         try {
-          const data = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyDsIbKSFnCPrNl2S-hmgmqL4FRq7BNKQV4`);
+          const data = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${API_KEY}`);
           const jsonBooksData = await data.json();
           return jsonBooksData;
         } catch (error) {
