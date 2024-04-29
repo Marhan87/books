dcee9ed31ddd1bfe9a9cbb2ea1d57646ddf1079e
Marhan87 <112327671+Marhan87@users.noreply.github.com>
2024-04-29 18:21:03 +0200

    Update errorBoundary.tsx
    Changed from console.log to console.error

--- src/utils/errorBoundary.tsx
+++ src/utils/errorBoundary.tsx
@@ -10,7 +10,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   }
 
   static getDerivedStateFromError(error: Error) {
-    console.log(error)
+    console.error(error)
     return { hasError: true };
   }
 
